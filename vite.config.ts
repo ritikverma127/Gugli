93d5c8ae3546a44037c0a5ed40b1782c02ac5add
Ritik Verma <ritikverma127>
2024-11-22 12:30:37 +0530

    "new changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base:"/Gugli/",
+  base: '/Gugli/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
